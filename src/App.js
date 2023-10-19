0614e4ea952ead1b624b9f7e18620768e1189963
HarshaMaria <harsha.maria@edstem.com>
2023-10-19 16:46:23 +0530

    Assessment

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import HomePage from './Components/HomePage';
+import Question from './Components/Question';
+import Results from './Components/Results';
+import './App.css';
+
+const App = () => {
+  const [gameState, setGameState] = useState('home');
+  const [score, setScore] = useState(0);
+
+  const startQuiz = () => {
+    setGameState('question');
+    setScore(0);
+  };
+
+  const endQuiz = () => {
+    setGameState('results');
+  };
+
+  return (
+    <div className="app">
+      {gameState === 'home' && <HomePage onStartQuiz={startQuiz} />}
+      {gameState === 'question' && <Question endQuiz={endQuiz} setScore={setScore} />}
+      {gameState === 'results' && <Results score={score} />}
+    </div>
+  );
+};
+
+export default App;
